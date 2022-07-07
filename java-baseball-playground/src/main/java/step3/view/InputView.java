289fc8bd9c6ad1973820835a0ed30d8b6393eba0
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-07-07 16:49:17 +0900

    refactor: 사용자 입력값 받아오도록 InputView 수정

--- java-baseball-playground/src/main/java/step3/view/InputView.java
+++ java-baseball-playground/src/main/java/step3/view/InputView.java
@@ -1,9 +1,49 @@
 package step3.view;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class InputView {
 
-    public void printGuide() {
+    private final Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public List<Integer> inputBaseballNumbers() {
         System.out.print("숫자를 입력해 주세요 : ");
+        String inputNumbers = scanner.nextLine();
+        while (!isValidInputNumbers(inputNumbers)) {
+            inputNumbers = scanner.nextLine();
+        }
+        return Arrays.stream(inputNumbers.split("")).map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isValidInputNumbers(String inputNumbers) {
+        try {
+            validateInputNumbers(inputNumbers);
+            return true;
+        } catch (IllegalArgumentException err) {
+            System.out.print("숫자를 다시 입력해 주세요.");
+            return false;
+        }
+    }
+
+    private void validateInputNumbers(String inputNumbers) {
+        if (inputNumbers.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+        boolean isDuplicated = inputNumbers.chars()
+                .filter(numb -> '1' <= numb && numb <= '9')
+                .distinct()
+                .count() != 3;
+        if (isDuplicated) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public void printWin() {

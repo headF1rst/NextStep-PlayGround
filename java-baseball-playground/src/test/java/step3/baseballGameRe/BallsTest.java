9e6574918e91a7378243598b8dd4193c2b17348b
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-07-15 00:27:05 +0900

    범위 밖의 공 숫자가 주어졌을때 예외발생 테스트 추가

--- java-baseball-playground/src/test/java/step3/baseballGameRe/BallsTest.java
+++ java-baseball-playground/src/test/java/step3/baseballGameRe/BallsTest.java
@@ -2,7 +2,10 @@ package step3.baseballGameRe;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BallsTest {
@@ -11,7 +14,17 @@ class BallsTest {
     @Test
     void ball() {
         BaseBallGameMachine baseBallGameMachine = new BaseBallGameMachine();
+        Balls balls = Balls.generateRandomBalls();
         BallStatus status = baseBallGameMachine.play(Ball.valueOf(1));
-        assertEquals(status, BallStatus.BALL);
+        assertThat(status).isEqualTo(BallStatus.BALL);
+    }
+
+    @DisplayName("1 ~ 9범위 밖의 공 숫자가 주어졌을때 예외발생 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 10})
+    void outOfRangeBallNumber(int input) {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> Ball.valueOf(input));
+        assertThat("1 ~ 9 안의 값이어야 합니다.").isEqualTo(exception.getMessage());
     }
 }

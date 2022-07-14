2aedd6d3ebd05bb7d203a567778854b435fdbb6d
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-07-15 00:25:49 +0900

    test: 볼인지 확인하는 테스트 추가

--- java-baseball-playground/src/test/java/step3/baseballGameRe/BallsTest.java
+++ java-baseball-playground/src/test/java/step3/baseballGameRe/BallsTest.java
@@ -1,19 +1,17 @@
 package step3.baseballGameRe;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BallsTest {
 
+    @DisplayName("볼인지 확인하는 테스트")
     @Test
     void ball() {
-        Balls balls = Balls.generateRandomBalls();
-
+        BaseBallGameMachine baseBallGameMachine = new BaseBallGameMachine();
+        BallStatus status = baseBallGameMachine.play(Ball.valueOf(1));
+        assertEquals(status, BallStatus.BALL);
     }
-
 }
